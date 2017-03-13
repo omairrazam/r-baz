ed7d43efc9e5dfb0a46bd774094e7f1527f3f549
Omair <omair@gems.techverx.com>
2017-03-13 23:17:10 +0500

    migrations added again

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170307186057) do
+ActiveRecord::Schema.define(version: 20170313181833) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",                      null: false
