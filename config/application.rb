6f4027f43d8edb8123d66e75ddf023a66ebd551d
Omair <omair@gems.techverx.com>
2017-03-14 00:23:46 +0500

    vendor folder added to assets path

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module Bazar
       end
     end
 
+    config.assets.paths << Rails.root.join("vendor", "assets", "images")
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
