243beabe7719f5e35b1349a42c2b49d442629526
Omair <omair@gems.techverx.com>
2017-05-06 10:17:57 +0500

    sale price override fixed

--- app/overrides/add_sale_price_to_product_edit.rb
+++ app/overrides/add_sale_price_to_product_edit.rb
@@ -1,11 +1,11 @@
 Deface::Override.new(virtual_path: 'spree/admin/products/_form',
                      name: 'add_sale_price_to_product_edit',
-                     insert_after: "erb[loud]:contains('text_field :price')",
-                     text: "
-    <%= f.field_container :sale_price do %>
-      <%= f.label :sale_price, raw(Spree.t(:sale_price) + content_tag(:span, ' *')) %>
-      <%= f.text_field :sale_price, value:
-        number_to_currency(@product.sale_price, unit: '') %>
-      <%= f.error_message_on :sale_price %>
-    <% end %>
-  ")
+                     insert_after: "[data-hook='admin_product_form_price']",
+                     text: "<div data-hook='admin_product_form_sale_price'>
+<%= f.field_container :sale_price do %>
+<%= f.label :sale_price, class: 'required' %>
+<%= render '/spree/admin/shared/number_with_currency', f: f, amount_attr: :sale_price, currency: @product.find_or_build_master.default_price.currency %>
+<%= f.error_message_on :sale_price %>
+<% end %>
+</div>"
+                     )
