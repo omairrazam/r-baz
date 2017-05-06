8bfbe4bdf4abfc1f64724039059bdff6b9f44597
Omair <omair@gems.techverx.com>
2017-05-06 08:38:02 +0500

    sale price functionality complete - it is yet to be isolated completely in extension

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -1,57 +1,68 @@
 Spree::BaseHelper.module_eval do
+  def display_price(product_or_variant)
+    #binding.pry
+    product_or_variant.price_for(current_pricing_options).to_html
+  end
 
-    def taxons_tree_product_show(root_taxon, current_taxon, max_level = 1)
-      return '' if max_level < 1 || root_taxon.leaf?
-      content_tag :div, class: 'list-group' do
-        taxons = root_taxon.children.map do |taxon|
-          css_class = (current_taxon && current_taxon.self_and_ancestors.include?(taxon)) ? 'list-group-item list-group-item-custom active' : 'list-group-item list-group-item-custom'
-          link_to(taxon.name, seo_url(taxon), class: css_class) + taxons_tree(taxon, current_taxon, max_level - 1)
+  def display_discounted_price(product_or_variant)
+    #binding.pry
+    product_or_variant.discounted_price_for(current_pricing_options).to_html
+  end
+
+  def taxons_tree_product_show(root_taxon, current_taxon, max_level = 1)
+    return '' if max_level < 1 || root_taxon.leaf?
+    content_tag :div, class: 'list-group' do
+      taxons = root_taxon.children.map do |taxon|
+        css_class = (current_taxon && current_taxon.self_and_ancestors.include?(taxon)) ? 'list-group-item list-group-item-custom active' : 'list-group-item list-group-item-custom'
+        link_to(taxon.name, seo_url(taxon), class: css_class) + taxons_tree(taxon, current_taxon, max_level - 1)
+      end
+      safe_join(taxons, "\n")
+    end
+  end
+
+  # def taxons_tree_product_show(root_taxon, current_taxon, max_level = 1)
+  #   return '' if max_level < 1 || root_taxon.leaf?
+  #   content_tag :ul do
+  #     taxons = root_taxon.children.map do |taxon|
+  #       concat(content_tag(:li,link_to(taxon.name, seo_url(taxon)) )) + taxons_tree(taxon, current_taxon, max_level - 1)
+  #     end
+  #     #safe_join(taxons, "\n")
+  #   end
+  # end
+
+  def taxons_home_tree(root_taxon, current_taxon, max_level = 1)
+    return '' if max_level < 1 || root_taxon.leaf?
+    count = 1;
+    taxons = root_taxon.children.map do |taxon|
+      content_tag :div, class: 'item' do
+        #css_class = (current_taxon && current_taxon.self_and_ancestors.include?(taxon)) ? 'list-group-item active' : 'list-group-item'
+        l = link_to(seo_url(taxon)) do
+          image_tag("spree/frontend/pic#{count}.jpg", :class => "img-responsive")+div_run_tag(taxon.name)
         end
-        safe_join(taxons, "\n")
+        count+=1
+        l + taxons_tree(taxon, current_taxon, max_level - 1)
       end
     end
+    safe_join(taxons, "")
+  end
 
-    # def taxons_tree_product_show(root_taxon, current_taxon, max_level = 1)
-    #   return '' if max_level < 1 || root_taxon.leaf?
-    #   content_tag :ul do
-    #     taxons = root_taxon.children.map do |taxon|
-    #       concat(content_tag(:li,link_to(taxon.name, seo_url(taxon)) )) + taxons_tree(taxon, current_taxon, max_level - 1)
-    #     end
-    #     #safe_join(taxons, "\n")
-    #   end
-    # end
-
-    def taxons_home_tree(root_taxon, current_taxon, max_level = 1)
-      return '' if max_level < 1 || root_taxon.leaf?
-		  count = 1;
-		  taxons = root_taxon.children.map do |taxon|
-  			content_tag :div, class: 'item' do
-  		 		#css_class = (current_taxon && current_taxon.self_and_ancestors.include?(taxon)) ? 'list-group-item active' : 'list-group-item'
-  		  		l = link_to(seo_url(taxon)) do
-  		  		  image_tag("spree/frontend/pic#{count}.jpg", :class => "img-responsive")+div_run_tag(taxon.name)
-  		  		end
-  		  		count+=1
-  		  		l + taxons_tree(taxon, current_taxon, max_level - 1) 
-  			end
-		  end
-		  safe_join(taxons, "")
+  def div_run_tag(title)
+    content_tag(:div, class: 'run') do
+      concat(content_tag(:i))
+      concat(content_tag(:p, title))
     end
+  end
 
-    def div_run_tag(title)
-    	content_tag(:div, class: 'run') do 
-    		concat(content_tag(:i))
-    		concat(content_tag(:p, title))
-    	end 
-    end
-
-    def taxons_tree_footer(root_taxon, current_taxon, max_level = 1)
-      return '' if max_level < 1 || root_taxon.leaf?
-      content_tag :ul, class: 'run-grid' do
-        taxons = root_taxon.children.map do |taxon|
-        	concat(content_tag(:li,link_to(taxon.name, seo_url(taxon)) )) + taxons_tree(taxon, current_taxon, max_level - 1)
-        end
-        #safe_join(taxons, "\n")
+  def taxons_tree_footer(root_taxon, current_taxon, max_level = 1)
+    return '' if max_level < 1 || root_taxon.leaf?
+    content_tag :ul, class: 'run-grid' do
+      taxons = root_taxon.children.map do |taxon|
+        concat(content_tag(:li, link_to(taxon.name, seo_url(taxon)))) + taxons_tree(taxon, current_taxon, max_level - 1)
       end
+      #safe_join(taxons, "\n")
     end
+  end
+
+
 
 end
